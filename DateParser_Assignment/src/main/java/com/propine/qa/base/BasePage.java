b2011a128a11c01a67c7af67240c463c12739f77
Anurag Bharti <Anurag Bharti@DESKTOP-L7AGLPD>
2020-07-03 09:50:35 +0530

    1.added validation for day, month, year 2.removed junk codes

--- DateParser_Assignment/src/main/java/com/propine/qa/base/BasePage.java
+++ DateParser_Assignment/src/main/java/com/propine/qa/base/BasePage.java
@@ -18,23 +18,22 @@ public class BasePage {
 	public WebDriver initializeDriver() throws IOException {
 
 
-//		prop = new Properties();
-//		FileInputStream fis=  new FileInputStream("C:\\Users\\Anurag Bharti\\eclipse-workspace\\"
-//				+ "DateParser_Assignment\\src\\main\\java\\com\\propine\\qa\\config\\file.properties");
-//		prop.load(fis);
-//
-//		System.setProperty("webdriver.chrome.driver", "/D:/drivers/for chrome version 83/chromedriver_win32/chromedriver.exe");
-//		String browsername=prop.getProperty("browser");
-//		if (browsername.equals("chrome")) {
-//			driver= new ChromeDriver();
-//		}
-//		//driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-//		driver.manage().window().maximize();
-//		driver.manage().deleteAllCookies();
-//		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS); //fetching from testUtil class
-//		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
-//		
-//		driver.get(prop.getProperty("url"));
+		prop = new Properties();
+		FileInputStream fis=  new FileInputStream("C:\\Users\\Anurag Bharti\\git\\DateParser\\DateParser_Assignment\\src\\main\\java\\com\\propine\\qa\\config\\file.properties");
+		prop.load(fis);
+
+		System.setProperty("webdriver.chrome.driver", "/D:/drivers/for chrome version 83/chromedriver_win32/chromedriver.exe");
+		String browsername=prop.getProperty("browser");
+		if (browsername.equals("chrome")) {
+			driver= new ChromeDriver();
+		}
+		//driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS); //fetching from testUtil class
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("url"));
 		return null;
 	}
 
--- DateParser_Assignment/src/test/java/com/propine/qa/test/LandingPageTest.java
+++ DateParser_Assignment/src/test/java/com/propine/qa/test/LandingPageTest.java
@@ -1,162 +1,122 @@
 package com.propine.qa.test;
-import java.text.SimpleDateFormat;
+
+import java.io.IOException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.time.Year;
-import java.time.chrono.IsoChronology;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.ResolverStyle;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalField;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TimeZone;
-
-import javax.swing.text.DateFormatter;
+import java.util.Locale;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.util.datetime.Format;
-import org.openqa.selenium.By;
-import org.testng.annotations.AfterMethod;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.google.gson.Gson;
 import com.propine.qa.base.BasePage;
 import com.propine.qa.libraries.ReadExcelLibrary;
 import com.propine.qa.pages.LandingPage;
 
-public class LandingPageTest extends BasePage{
+public class LandingPageTest extends BasePage {
+	public static Logger log = LogManager.getLogger(BasePage.class.getName());
 
-	public static Logger log=LogManager.getLogger(BasePage.class.getName());
+	public Object[][] Test_Data;
+	public String flag=null;
 
-	public Object [][] Test_Data;
-	//	@BeforeTest
-	//	public void initialize() throws IOException{
-	//		driver=initializeDriver();
-	//	}
+	String expdate = null;
+	String expmonth=null ;
+	String expyear=null;
+
+	@BeforeTest
+	public void initialize() throws IOException{
+		driver=initializeDriver();
+	}
 
 	@Test(dataProvider="dateParserTestData")
-	public void DateParserTest(String Test_Data ) {
-		//	ReadExcelLibrary obje= new ReadExcelLibrary();
-		//obje.getData(sheetnumber, row, column)
-		//		driver.get(prop.getProperty("url"));
-		//		driver.manage().window().maximize();
-		//		log.debug("window is maximized");
+	public void DateParser(String Test_Data ) {
 		try {
+
 			DateTimeFormatter ALL_POSSIBLE_DATE_FORMAT = new DateTimeFormatterBuilder()
-					.appendOptional(DateTimeFormatter.ofPattern("DD-MMM-YYYY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD-MM-YY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD-MMMM-YYYY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD-MMM-YYYY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD-MMM-YY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD-MMMM-YYYY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD-MM-YYYY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD-MM-YY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD/MM/YYYY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD/MM/YY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD MM YYYY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD MM YY"))
-					.appendOptional(DateTimeFormatter.ofPattern("MM DD YY"))
-					.appendOptional(DateTimeFormatter.ofPattern("DD/M-YYYY")).toFormatter();
-			TemporalAccessor ta = ALL_POSSIBLE_DATE_FORMAT.parse(Test_Data.toString());
-			System.out.println("Formatted "+  ta  + " " + Test_Data.toString());
-//			Gson gson = new Gson();
-//			Map<String ,String> attributes = gson.fromJson(gson.toJson(ta.toString()),Map.class);
-//			System.out.println(attributes.toString());
-			String [] arr = ta.toString().split(",");
-//			for(String d : arr) {
-////				System.out.println(d);
-//				String[] entry =  d.split("=");
-//				System.out.println(entry[1].toString());	
-//			
-//			}
-			
+					.appendOptional(DateTimeFormatter.ofPattern("dd-MMMM-uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd-MMMM-uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd-MMM-uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd-MMM-uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd-MM-uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd-MM-uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("MMMM-dd-uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("MMMM-dd-uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("MMM-dd-uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("MMM-dd-uu"))					
+					.appendOptional(DateTimeFormatter.ofPattern("MM-dd-uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("MM-dd-uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd/MM/uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd/MM/uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd/MMM/uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd MM uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd MM uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("MM dd uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("MM/dd/uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("MM/dd/uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd-MM-uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd/MM/uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd MMMM-uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("d/M-uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd/MMM/uu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd/MMMM/uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd-MMMM-uuuu"))
+					.appendOptional(DateTimeFormatter.ofPattern("dd/M-uuuu")).toFormatter(Locale.ENGLISH);
+
+			LocalDate parseddate = LocalDate.parse(Test_Data.toString(), ALL_POSSIBLE_DATE_FORMAT.withResolverStyle(ResolverStyle.STRICT));
+			//System.out.println(parseddate.getYear());
+			//System.out.println(parseddate.getMonthValue());
+			expdate= String.valueOf(parseddate.getDayOfMonth());
+			expmonth = String.valueOf(parseddate.getMonth());
+			expyear = String.valueOf(parseddate.getYear());
+			System.out.println("Day= "+parseddate.getDayOfMonth() +", " + "Month="+parseddate.getMonthValue()+", "+ "Year="+parseddate.getYear());
+			flag = "Valid Date";
 		}
 		catch(Exception e) {
-			System.out.println("Unknown" + Test_Data.toString());
+			//e.printStackTrace();
+			System.out.println("Invalid date/format :" + " " + Test_Data.toString());
+			flag = "Invalid date";
 		}
 
-		//		try {
-		//			
-		//			TemporalAccessor ta = DateTimeFormatter.ofPattern("DD-MMM-YYYY").withResolverStyle(ResolverStyle.LENIENT).withChronology(IsoChronology.INSTANCE).parse(data.toString());
-		//			System.out.println(ta +" Actual:"+ data.toString());
-		//		}catch(Exception e) {
-		//			System.out.println("Unknown Format");
-		//		}
-		////		SimpleDateFormat smf;
-		//		Date d = null;
-		//		try {
-		//			smf = new SimpleDateFormat();
-		//			smf.setLenient(true);
-		//			smf.applyPattern("DD-MM-YYYY");
-		//			smf.parse(data);
-		//			System.out.println("Pattern1 Matched" +data);
-		//		}
-		//		catch (Exception ex) {
-		//			System.out.println("pattern not found");
-		//		}
-		//		try {
-		//			smf = new SimpleDateFormat();
-		//			smf.setLenient(true);
-		//			smf.applyPattern("DD-MM-YY");
-		//			smf.parse(data);
-		//			System.out.println("Pattern2 Matched" +data);
-		//
-		//		} catch (Exception ex) {
-		//			System.out.println("pattern not found");
-		//			// do something for invalid dateformat
-		//		}
-		//		LandingPage lp= new LandingPage();
-		//		lp.getDate(data);
-		//driver.findElement(By.xpath("//input[@class='form-control']")).sendKeys(data);
-		//		log.debug("data is inputed "+data);
-
-		//driver.findElement(By.xpath("//input[@type='submit']")).click();
-		//log.debug("clicked");
-		//		if (!lp.viewResults().equals("Invalid Date")) {
-		//			String[] text=lp.viewResults().split(" ");
-		//			String month = text[1];
-		//			String date = text[2];
-		//			String year = text[3];
-		//		}
-
-		//	String text = driver.findElement(By.xpath("//body//div[2]//div/div[2]//div")).getText();
-		//log.info("Results: " +text);
+		LandingPage lp= new LandingPage();
+		lp.getDate(Test_Data.toString());
+		log.debug("data is inputed "+Test_Data);
+		if(!lp.viewResults().equals("Invalid date")) {
+			String[] date = lp.viewResults().split(" ");
+			Assert.assertEquals(expdate, date[2],"Invalid Date");
+			Assert.assertEquals(expmonth, date[1],"Invalid Month");
+			Assert.assertEquals(expyear,date[3],"Invalid Year");
+		}else {
+			Assert.assertEquals(flag, lp.viewResults().equals("Invalid date"),"Error !!!");
+		}
 
 	}
 
-	@DataProvider(name="dateParserTestData")
-	public Object[][] passData() 
-	{
-		ReadExcelLibrary config= new ReadExcelLibrary("C:\\Users\\Anurag Bharti\\eclipse-workspace\\DateParser_Assignment"
-				+ "\\src\\main\\java\\com\\propine\\testdata\\Propine_Test_Data.xlsx");
-
-		int rows=config.getRowCount(0)+1;
+	@DataProvider(name = "dateParserTestData")
+	public Object[][] passData() {
+		ReadExcelLibrary config = new ReadExcelLibrary("C:\\Users\\Anurag Bharti\\git\\DateParser\\DateParser_Assignment\\src\\main\\java\\com\\propine\\testdata\\Propine_Test_Data.xlsx");
+		int rows = config.getRowCount(0) + 1;
 		System.out.println(rows);
 
-		Test_Data= new Object[rows-1][1];
-		int j=0;
-		for (int i=1;i<rows; i++) 
-		{	
+		Test_Data = new Object[rows - 1][1];
+		int j = 0;
+		for (int i = 1; i < rows; i++) {
 			Test_Data[j][0] = config.getData(0, i, 6);
 			j++;
 		}
 		return Test_Data;
 	}
 
-	//	@AfterTest
-	//	public void teardown() 
-	//	{
-	//		driver.close();
-	//		driver=null;
-	//	}
+	//	 @AfterTest
+	//	 public void teardown()
+	//	 {
+	//	 driver.close();
+	//	 driver=null;
+	//	 }
 
 }
